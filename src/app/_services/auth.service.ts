1de9073f6d517dee3c1bef441fa8207a8d91b6db
Steven Del Rio <delrio.steven@gmail.com>
2018-04-11 16:46:48 -0500

    Added 3rd party components

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -4,19 +4,23 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import { Observable } from 'rxjs/Observable';
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
     baseUrl = 'http://localhost:5000/api/auth/';
     userToken: any;
+    decodedToken: any;
+    jwtHelper: JwtHelper = new JwtHelper();
 
 constructor(private http: Http) { }
 
 login(model: any){
     return this.http.post(this.baseUrl + 'login', model, this.requestOptions()).map((response: Response) => {
         const user = response.json();
-        if (user) {
+        if (user && user.tokenString) {
             localStorage.setItem('token', user.tokenString);
+            this.decodedToken = this.jwtHelper.decodeToken(user.tokenString);
             this.userToken = user.tokenString;
         }
     }).catch(this.handleError);
@@ -24,6 +28,10 @@ login(model: any){
 
 register(model: any) {
     return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).catch(this.handleError);
+}
+
+loggedIn() {
+    return tokenNotExpired('token');
 }
 
 private requestOptions() {
@@ -40,7 +48,7 @@ private handleError(error: any) {
     const serverError = error.json();
     let modelStateErrors = '';
     if (serverError) {
-        for (const key in serverError){
+        for (const key in serverError) {
             if (serverError[key]) {
                 modelStateErrors += serverError[key] + '\n';
             }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { AlertifyService } from './_services/alertify.service';
 
 
 
@@ -22,10 +24,12 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
-    AuthService
+    AuthService,
+    AlertifyService
   ],
   bootstrap: [AppComponent]
 })
